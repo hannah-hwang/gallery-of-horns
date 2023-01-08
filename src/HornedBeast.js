cdb2387213df62ef6557ad835d6fad9c58385ca0
Hannah Hwang <hannah.hwang1125@gmail.com>
2023-01-07 16:46:26 -0800

    finished lab01

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -1,1 +1,15 @@
 import React from "react";
+
+class HornedBeast extends React.Component {
+    render(){
+        return(
+            <>
+            <h2>{this.props.title}</h2>
+            <img src={this.props.imageUrl} alt={this.props.description} title={this.props.title}/>
+            <p>{this.props.description}</p>
+            </>
+        )
+    }
+}
+
+export default HornedBeast
--- src/Main.js
+++ src/Main.js
@@ -1,11 +1,13 @@
 import React from "react";
-import HornedBeast from "HornedBeast";
+import HornedBeast from "./HornedBeast";
 
-class Main extends React.component{
-    render(){
-        return(
+class Main extends React.Component {
+    render() {
+        return (
             <>
-            <HornedBeast/>
+                <HornedBeast title={"UniWhale"} imageUrl={"http://3.bp.blogspot.com/_DBYF1AdFaHw/TE-f0cDQ24I/AAAAAAAACZg/l-FdTZ6M7z8/s1600/Unicorn_and_Narwhal_by_dinglehopper.jpg"} description={"A unicorn and a narwhal nuzzling their horns"} />
+                <HornedBeast title={"Rhino Family"} imageUrl={"https://images.unsplash.com/photo-1512636618879-bbe79107e9e3?ixlib=rb-0.3.5&ixid=eyJhcHBfaWQiOjEyMDd9&s=bd9460ee6d1ddbb6b1ca7be86dfc4590&auto=format&fit=crop&w=1825&q=80"} description={"Parent rhino with two babies"} />
+                <HornedBeast title={"Unicorn Head"} imageUrl={"https://www.dhresource.com/0x0s/f2-albu-g5-M00-1A-11-rBVaI1hsIIiALxKzAAIHjSU3VkE490.jpg/wholesale-halloween-costume-prop-unicorn.jpg"} description={"Someone wearing a very silly unicorn head mask"} />
             </>
         )
     }
