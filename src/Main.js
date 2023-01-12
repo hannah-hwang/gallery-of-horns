c08508ea5b5e9c2fcb7ff0e41cd36963cbd090d1
Hannah Hwang <hannah.hwang1125@gmail.com>
2023-01-12 15:49:02 -0800

    imported bootstrap and added cards

--- src/Main.js
+++ src/Main.js
@@ -1,17 +1,18 @@
 import React from "react";
 import HornedBeast from "./HornedBeast";
 import data from "./data.json"
+import "./App.css"
 
 class Main extends React.Component {
     render() {
         return (
-            <>
+            <main>
                 {data.map((hornedBeast, idx) => <HornedBeast title={hornedBeast.title}
                     image_url={hornedBeast.image_url}
                     description={hornedBeast.description}
                     idx={idx}
-                    />)}
-            </>
+                />)}
+            </main>
         )
     }
 }
