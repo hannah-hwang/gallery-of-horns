63035d4cbd36ffafa2ec612afd5d77881ed058fb
Hannah Hwang <hannah.hwang1125@gmail.com>
2023-01-12 15:09:33 -0800

    can favorite

--- src/HornedBeast.js
+++ src/HornedBeast.js
@@ -4,28 +4,31 @@ class HornedBeast extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            clickedFavorite: false
-        }
+            clickedFavorite: false,
+            favoritedNumber: 0
+        };
     }
 
     handleFavorite = () => {
         this.setState({
-            clickedFavorite: true
+            clickedFavorite: this.state.clickedFavorite ? false : true
         })
-    }
+    };
 
     render() {
         return (
-            <>
+            <div className="hornedBeast">
                 <h2>{this.props.title}</h2>
+                <p>{this.props.description}</p>
                 <img src={this.props.image_url}
                     alt={this.props.description}
                     title={this.props.title}
-                    onClick=(this.handleFavorite)
-                    />
-                <p>{this.props.description}</p>
-                {this.clickedFavorite ? <p>Favorited: 💜</p> : <p>Favorited: 🖤</p>}
-            </>
+                    height={300}
+                    onClick={this.handleFavorite}
+                />
+                {this.state.clickedFavorite ? <p>Favorited: 💜</p> : <p>Favorited: 🖤</p>}
+                <p>Favorites: {this.state.favoritedNumber}</p>
+            </div>
         )
     }
 }
